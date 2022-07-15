69153760ecabfa29ba71d858e7f9a73f23422ce6
Asadbek <asadbek@kali.kali>
2022-07-15 06:01:52 -0400

    add

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(express.json())
 app.use(cors())
 
 
-mongoose.connect("mongodb://localhost:127.0.0.1:27017/uzb" ,
-    {
-        useNewUrlParser:true ,
-     useUnifiedTopology:true}
-)
+// mongoose.connect("mongodb://localhost:127.0.0.1:27017/uzb" ,
+//     {
+//         useNewUrlParser:true ,
+//      useUnifiedTopology:true}
+// )
 .then(() => {
     console.log('Bazaga ulandi')
 })
